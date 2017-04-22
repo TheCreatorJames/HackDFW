5117894fd533233b8be086b2e4a4e055bf690100
Chris Gloom <christopherevancrawford@gmail.com>
2017-04-22 15:46:08 -0500

    should fix horrible white noise triggering

--- js/soundGeneration.js
+++ js/soundGeneration.js
@@ -3,9 +3,17 @@ var audiocntx = new window.AudioContext()
 // hz values of a c major scale
 var cMajor = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]
 var cPenta = [261.63, 293.66, 329.63, 392.00, 440.00, 523.25]
+var abcCPenta = ['C,', 'D', 'E', 'G', 'A', 'C\'']
+
+var instrument = new Instrument({wave: 'sine', cutoff: 1000, release: 2, detune: 0})
+var shimmer = new Instrument({wave: 'sawtooth', cutoff: 1000, release: 4, detune: 0})
 window.addEventListener('keydown', function (e) {
-  sineNote(0.5, 0)
-  noiseOn()
+  var char = abcCPenta[Math.floor((Math.random() * 6))]
+  instrument.play('[d2f2][ce][df]')
+  console.log(char)
+
+  // sineNote(0.5, 0)
+  // noiseOn()
 })
 var sineNote = function (drivingSpeed, moduloValue) {
   var oscillator = audiocntx.createOscillator()
@@ -36,9 +44,9 @@ var brownNoise = (function () {
 })()
 
 var noiseOn = function () {
-  brownNoise.connect(audiocntx.destination)
-  var gainNode = audiocntx.createGain()
-  gainNode.gain.value = 0.5
-  gainNode.connect(audiocntx.destination)
-  gainNode.gain.setTargetAtTime(0.001, audiocntx.currentTime + 2, 0.5)
+  var brownGain = audiocntx.createGain()
+  brownGain.gain.value = 0.5
+  brownGain.connect(audiocntx.destination)
+  brownNoise.connect(brownGain)
+  brownGain.gain.setTargetAtTime(0.001, audiocntx.currentTime + 2, 0.5)
 }
