7f8d92a508a64809cef6795ef145d52b91592903
Jesse Mitchell <thecreatorjames@live.com>
2017-04-23 02:58:07 -0500

    Additions

--- js/js/selectJS.js
+++ js/js/selectJS.js
@@ -1,10 +1,24 @@
 function selectFileM()
 {
     var el = document.getElementById("selM");
+    if(el.value == "Upload")
+    {
+        var input = $(document.createElement('input'));
+        input.attr("type", "file");
+        input.trigger('click'); // opening dialog
+    }
+    el.selectedIndex = "0";
 }
 
 function selectFileD()
 {
     var el = document.getElementById("selD");
-    alert(el.value);
+    if(el.value == "Upload")
+    {
+        var input = $(document.createElement('input'));
+        input.attr("type", "file");
+        input.trigger('click'); // opening dialog
+    }
+    
+    el.selectedIndex = "0";
 }
